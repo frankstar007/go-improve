90fa03ca80986c9f935502e4c2f93c9faf448a36
frankstar <yehongxing@meituan.com>
2021-03-09 21:06:56 +0800

    add .

---
+++ go.mod
@@ -0,0 +1,3 @@
+module src/frank-chat
+
+require github.com/go-sql-driver/mysql v1.5.0 // indirect
---
+++ src/frank-chat/main.go
@@ -0,0 +1,9 @@
+/**
+ * @Author: frankstar
+ * @Email: frankstar007@163.com
+ * @Date: 2020-07-22 11:30
+ * @Project : go-improve
+ * @Desc: 
+*/
+
+package mian
