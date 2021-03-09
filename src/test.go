8b801f6ba5656bd358a005105422db7123fcd3e1
frankstar <yehongxing@meituan.com>
2021-03-09 21:11:06 +0800

    add .

---
+++ src/test.go
@@ -0,0 +1,15 @@
+/**
+ * @Author: frankstar
+ * @Email: frankstar007@163.com
+ * @Date: 2021-03-09 21:09
+ * @Project : go-improve
+ * @Desc: 
+*/
+
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("hello frankstar")
+}
